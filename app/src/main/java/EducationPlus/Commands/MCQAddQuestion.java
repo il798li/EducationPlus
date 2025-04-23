f01e8fba57ec19fe4d652f9a4450e5bda898daee
il798li <92280179+il798li@users.noreply.github.com>
2025-04-23 12:32:12 -0700

    added mcq id option to slash command structure

--- app/src/main/java/EducationPlus/Commands/MCQAddQuestion.java
+++ app/src/main/java/EducationPlus/Commands/MCQAddQuestion.java
@@ -1,16 +1,18 @@
 package EducationPlus.Commands;
 
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.build.Commands;
 import net.dv8tion.jda.api.interactions.commands.build.SlashCommandData;
 public class MCQAddQuestion {
     public static SlashCommandData slashCommandData () {
         final SlashCommandData slashCommandData = Commands.slash ("mcq-add-question", "Adds a question to your Multiple-Choice Question Test.");
+        slashCommandData.addOption (OptionType.STRING, "mcq-id", "What is the ID of your MCQ Test?");
         slashCommandData.addOption (OptionType.STRING, "question", "What should your question ask?");
         slashCommandData.addOption (OptionType.ATTACHMENT, "image", "What image should your question display?");
         slashCommandData.addOption (OptionType.STRING, "correct", "What is the correct answer to your question?");
         for (short index = 1; index <= 3; index += 1) {
-            slashCommandData.addOption(OptionType.STRING, "wrong-" + index, "What is a wrong answer to your question", index == 1);
+            slashCommandData.addOption (OptionType.STRING, "wrong-" + index, "What is a wrong answer to your question", index == 1);
         }
         return slashCommandData;
     }
