52076bc1fca2e7f3ad1df229776497e3ce0d21ba
il798li <92280179+il798li@users.noreply.github.com>
2025-04-28 13:36:42 -0700

    added required options for mcq-add-question command

--- app/src/main/java/EducationPlus/Commands/MCQAddQuestion.java
+++ app/src/main/java/EducationPlus/Commands/MCQAddQuestion.java
@@ -12,10 +12,10 @@ import net.dv8tion.jda.api.interactions.commands.build.SlashCommandData;
 public class MCQAddQuestion {
     public static SlashCommandData slashCommandData () {
         final SlashCommandData slashCommandData = Commands.slash ("mcq-add-question", "Adds a question to your Multiple-Choice Question Test.");
-        slashCommandData.addOption (OptionType.STRING, "mcq-id", "What is the ID of your MCQ Test?");
-        slashCommandData.addOption (OptionType.STRING, "question", "What should your question ask?");
+        slashCommandData.addOption (OptionType.STRING, "mcq-id", "What is the ID of your MCQ Test?", true);
+        slashCommandData.addOption (OptionType.STRING, "question", "What should your question ask?", true);
         slashCommandData.addOption (OptionType.ATTACHMENT, "image", "What image should your question display?");
-        slashCommandData.addOption (OptionType.STRING, "correct", "What is the correct answer to your question?");
+        slashCommandData.addOption (OptionType.STRING, "correct", "What is the correct answer to your question?", true);
         for (short index = 1; index <= 3; index += 1) {
             slashCommandData.addOption (OptionType.STRING, "wrong-" + index, "What is a wrong answer to your question", index == 1);
         }
