c1c42bb1d65d183887176e0fd62595444cf75ee8
il798li <92280179+il798li@users.noreply.github.com>
2025-04-29 13:38:09 -0700

    finished with adding a new qusetions, except for inserting in between two other questions

--- app/src/main/java/EducationPlus/Commands/MCQAddQuestion.java
+++ app/src/main/java/EducationPlus/Commands/MCQAddQuestion.java
@@ -1,14 +1,29 @@
 package EducationPlus.Commands;
 
 import EducationPlus.Classes.Helpers.Test;
+import EducationPlus.Listeners.Ready;
 import EducationPlus.Utility.DiscordUtility;
 import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.User;
+import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.interactions.commands.OptionMapping;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.build.Commands;
 import net.dv8tion.jda.api.interactions.commands.build.SlashCommandData;
+import net.dv8tion.jda.api.requests.restaction.MessageCreateAction;
+import net.dv8tion.jda.api.utils.AttachmentProxy;
+import net.dv8tion.jda.api.utils.FileUpload;
+import net.dv8tion.jda.api.utils.messages.MessageCreateBuilder;
+import net.dv8tion.jda.api.utils.messages.MessageCreateData;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.concurrent.CompletableFuture;
+
 public class MCQAddQuestion {
     public static SlashCommandData slashCommandData () {
         final SlashCommandData slashCommandData = Commands.slash ("mcq-add-question", "Adds a question to your Multiple-Choice Question Test.");
@@ -50,5 +65,54 @@ public class MCQAddQuestion {
             DiscordUtility.respond (slashCommandInteractionEvent, embedBuilder);
             return;
         }
+        final MessageCreateBuilder questionMessageCreateBuilder = new MessageCreateBuilder ();
+        {
+            final OptionMapping mcqQuestionOptionMapping = slashCommandInteractionEvent.getOption ("question");
+            final String mcqQuestion = mcqQuestionOptionMapping.getAsString ();
+            final OptionMapping mcqCorrectAnswerOptionMapping = slashCommandInteractionEvent.getOption ("correct");
+            final String mcqCorrectAnswer = mcqCorrectAnswerOptionMapping.getAsString ();
+            String content = mcqQuestion + "\n" + mcqCorrectAnswer;
+            for (int wrongAnswer = 1; wrongAnswer <= 3; wrongAnswer += 1) {
+                final OptionMapping mcqWrongAnswerOptionMapping = slashCommandInteractionEvent.getOption ("wrong-" + wrongAnswer);
+                if (mcqWrongAnswerOptionMapping != null) {
+                    content += "\n" + mcqWrongAnswerOptionMapping.getAsString ()
+                }
+            }
+            questionMessageCreateBuilder.setContent (content);
+        }
+        final OptionMapping questionImageOptionMapping = slashCommandInteractionEvent.getOption ("image");
+        if (questionImageOptionMapping != null) {
+            final Message.Attachment questionPageAttachment = questionImageOptionMapping.getAsAttachment ();
+            final AttachmentProxy attachmentProxy = questionPageAttachment.getProxy ();
+            final CompletableFuture<InputStream> inputStreamCompletableFuture = attachmentProxy.download ();
+            inputStreamCompletableFuture.complete (null);
+            final FileUpload fileUpload = FileUpload.fromStreamSupplier (
+                questionPageAttachment.getFileName () + questionPageAttachment.getFileExtension (),
+                inputStreamCompletableFuture.get ()
+            );
+            questionMessageCreateBuilder.addFiles (fileUpload);
+        }
+        final JDA jda = slashCommandInteractionEvent.getJDA ();
+        final TextChannel lessonPagesTextChannel = jda.getTextChannelById (Ready.lessonPagesTextChannelID);
+        final MessageCreateData questionMessageCreateData = questionMessageCreateBuilder.build ();
+        final MessageCreateAction questionMessageCreateAction = lessonPagesTextChannel.sendMessage (questionMessageCreateData);
+        questionMessageCreateAction.queue (questionMessage -> {
+            final Test.MCQ.Question newQuestion = Test.MCQ.Question.fromMessage (questionMessage);
+            final Test.MCQ.Question[] newMCQQuestions = new Test.MCQ.Question[mcq.questions.length + 1];
+            for (short index = 0; index < mcq.questions.length; index += 1) {
+                newMCQQuestions[index] = mcq.questions[index];
+            }
+            newMCQQuestions[mcq.questions.length] = newQuestion;
+            mcq.questions = newMCQQuestions;
+        });
     }
 }
+/*
+    if (lessonPageImageAttachment != null) {
+final AttachmentProxy lessonPageImageAttachmentProxy = lessonPageImageAttachment.getProxy ();
+final String lessonPageImageExtension = lessonPageImageAttachment.getFileExtension ();
+final FileUpload lessonPageImageFileUpload = FileUpload.fromStreamSupplier (lessonPageImageAttachment.getFileName (), () -> lessonPageImageAttachmentProxy.download ().join ());
+            lessonPageMessage.addFiles (lessonPageImageFileUpload);
+        }
+
+ */
