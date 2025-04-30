80efa0bf2edd81cdddf066ba6cb3ba48a2f60a98
il798li <92280179+il798li@users.noreply.github.com>
2025-04-30 12:36:40 -0700

    added basic check and started with execution of mcq attempt

--- app/src/main/java/EducationPlus/Commands/MCQ/MCQAttempt.java
+++ app/src/main/java/EducationPlus/Commands/MCQ/MCQAttempt.java
@@ -3,11 +3,16 @@ package EducationPlus.Commands.MCQ;
 import EducationPlus.Classes.Helpers.Test;
 import EducationPlus.Utility.DiscordUtility;
 import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.interactions.InteractionHook;
 import net.dv8tion.jda.api.interactions.commands.OptionMapping;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.build.Commands;
 import net.dv8tion.jda.api.interactions.commands.build.SlashCommandData;
+import net.dv8tion.jda.api.requests.restaction.WebhookMessageCreateAction;
+import net.dv8tion.jda.api.utils.messages.MessageCreateBuilder;
+import net.dv8tion.jda.api.utils.messages.MessageCreateData;
 public class MCQAttempt {
     public static SlashCommandData slashCommandData () {
         final SlashCommandData slashCommandData = Commands.slash ("mcq-attempt", "Attempts an MCQ Test to see your skills.");
@@ -24,9 +29,22 @@ public class MCQAttempt {
         if (mcq == null) {
             final EmbedBuilder embedBuilder = new EmbedBuilder ();
             embedBuilder.setTitle ("Error");
-            embedBuilder.setDescription ("I could not find a MCQ Test with ID:```" + mcqID + "```")
+            embedBuilder.setDescription ("I could not find a MCQ Test with ID:```" + mcqID + "```");
             DiscordUtility.respond(slashCommandInteractionEvent, embedBuilder);
             return;
         }
+        if (mcq.questions.length <= 0) {
+            final EmbedBuilder embedBuilder = new EmbedBuilder ();
+            embedBuilder.setTitle ("Error");
+            embedBuilder.setDescription ("The MCQ Test " + mcq.title + " has no questions.");
+            DiscordUtility.respond (slashCommandInteractionEvent, embedBuilder);
+            return;
+        }
+        final Test.MCQ.Question question = mcq.questions[0];
+        final MessageCreateBuilder messageCreateBuilder = mcq.messageCreateBuilder (0);
+        final MessageCreateData messageCreateData = messageCreateBuilder.build ();
+        final InteractionHook interactionHook = slashCommandInteractionEvent.getHook ();
+        final WebhookMessageCreateAction<Message> messageWebhookMessageCreateAction = interactionHook.sendMessage (messageCreateData);
+        messageWebhookMessageCreateAction.queue ();
     }
 }
