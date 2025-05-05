1de965000bb4b8b10cef4aa640d898dbcb4c737f
il798li <92280179+il798li@users.noreply.github.com>
2025-05-05 13:51:17 -0700

    added blank response handling for unrecognized string select interactions

--- app/src/main/java/EducationPlus/Listeners/StringSelectInteraction.java
+++ app/src/main/java/EducationPlus/Listeners/StringSelectInteraction.java
@@ -1,8 +1,13 @@
 package EducationPlus.Listeners;
 
 import EducationPlus.Commands.MCQ.MCQAttempt;
+import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.events.interaction.component.StringSelectInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.interactions.InteractionHook;
+import net.dv8tion.jda.api.requests.restaction.MessageEditAction;
+import net.dv8tion.jda.api.utils.messages.MessageEditBuilder;
+import net.dv8tion.jda.api.utils.messages.MessageEditData;
 import org.json.JSONObject;
 public class StringSelectInteraction extends ListenerAdapter {
     public void onStringSelectInteraction (final StringSelectInteractionEvent stringSelectInteractionEvent) {
@@ -13,6 +18,13 @@ public class StringSelectInteraction extends ListenerAdapter {
             case "mcq-attempt": {
                 MCQAttempt.stringSelectInteraction (stringSelectInteractionEvent);
                 break;
+            }
+            default: {
+                final Message message = stringSelectInteractionEvent.getMessage ();
+                final MessageEditBuilder messageEditBuilder = MessageEditBuilder.fromMessage (message);
+                final MessageEditData messageEditData = messageEditBuilder.build ();
+                final MessageEditAction messageEditAction = message.editMessage (messageEditData);
+                messageEditAction.queue ();
             }
         }
     }
