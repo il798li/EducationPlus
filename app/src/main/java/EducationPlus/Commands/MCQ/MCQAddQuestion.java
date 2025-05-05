f46bff494b76ea2f3040e2204ce081cd280603e6
il798li <92280179+il798li@users.noreply.github.com>
2025-05-05 13:47:19 -0700

    added responding with a success message for adding a question

--- app/src/main/java/EducationPlus/Commands/MCQ/MCQAddQuestion.java
+++ app/src/main/java/EducationPlus/Commands/MCQ/MCQAddQuestion.java
@@ -6,18 +6,24 @@ import EducationPlus.Utility.DiscordUtility;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.MessageEmbed;
 import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
+import net.dv8tion.jda.api.entities.emoji.Emoji;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.interactions.InteractionHook;
 import net.dv8tion.jda.api.interactions.commands.OptionMapping;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.build.Commands;
 import net.dv8tion.jda.api.interactions.commands.build.SlashCommandData;
+import net.dv8tion.jda.api.interactions.components.selections.StringSelectMenu;
 import net.dv8tion.jda.api.requests.restaction.MessageCreateAction;
+import net.dv8tion.jda.api.requests.restaction.WebhookMessageCreateAction;
 import net.dv8tion.jda.api.utils.AttachmentProxy;
 import net.dv8tion.jda.api.utils.FileUpload;
 import net.dv8tion.jda.api.utils.messages.MessageCreateBuilder;
 import net.dv8tion.jda.api.utils.messages.MessageCreateData;
+import org.json.JSONObject;
 
 import java.io.InputStream;
 import java.util.concurrent.CompletableFuture;
@@ -66,12 +72,13 @@ public class MCQAddQuestion {
             return;
         }
         final MessageCreateBuilder questionMessageCreateBuilder = new MessageCreateBuilder ();
+        String mcqQuestionText;
         {
             final OptionMapping mcqQuestionOptionMapping = slashCommandInteractionEvent.getOption ("question");
-            final String mcqQuestion = mcqQuestionOptionMapping.getAsString ();
+            mcqQuestionText = mcqQuestionOptionMapping.getAsString ();
             final OptionMapping mcqCorrectAnswerOptionMapping = slashCommandInteractionEvent.getOption ("correct");
             final String mcqCorrectAnswer = mcqCorrectAnswerOptionMapping.getAsString ();
-            String content = mcqQuestion + "\n" + mcqCorrectAnswer;
+            String content = mcqQuestionText + "\n" + mcqCorrectAnswer;
             for (int wrongAnswer = 1; wrongAnswer <= 3; wrongAnswer += 1) {
                 final OptionMapping mcqWrongAnswerOptionMapping = slashCommandInteractionEvent.getOption ("wrong-" + wrongAnswer);
                 if (mcqWrongAnswerOptionMapping != null) {
@@ -81,8 +88,13 @@ public class MCQAddQuestion {
             questionMessageCreateBuilder.setContent (content);
         }
         final OptionMapping questionImageOptionMapping = slashCommandInteractionEvent.getOption ("image");
+        final EmbedBuilder responseEmbedBuilder = new EmbedBuilder ();
         if (questionImageOptionMapping != null) {
             final Message.Attachment questionPageAttachment = questionImageOptionMapping.getAsAttachment ();
+            {
+                final String url = questionPageAttachment.getUrl ();
+                responseEmbedBuilder.setImage (url);
+            }
             final AttachmentProxy attachmentProxy = questionPageAttachment.getProxy ();
             final CompletableFuture <InputStream> inputStreamCompletableFuture = attachmentProxy.download ();
             final InputStream inputStream = inputStreamCompletableFuture.join ();
@@ -103,5 +115,30 @@ public class MCQAddQuestion {
             newMCQQuestions[mcq.questions.length] = newQuestion;
             mcq.questions = newMCQQuestions;
         });
+        responseEmbedBuilder.setTitle ("Success");
+        responseEmbedBuilder.setDescription ("Your question was successfully added to MCQ " + mcq.title + " with ID:```" + mcq.id + "```");
+        responseEmbedBuilder.addField ("Question", mcqQuestionText, false);
+        final MessageEmbed responseEmbed = responseEmbedBuilder.build ();
+        final MessageCreateBuilder responseMessageCreateBuilder = new MessageCreateBuilder ();
+        responseMessageCreateBuilder.setEmbeds (responseEmbed);
+        {
+            final JSONObject jsonObject = new JSONObject ();
+            jsonObject.put ("command", "mcq-add-question");
+            final String customID = jsonObject.toString ();
+            final StringSelectMenu.Builder stringSelectMenuBuilder = StringSelectMenu.create (customID);
+            final Test.MCQ.Question question = mcq.questions[mcq.questions.length - 1];
+            final Emoji whiteCheckMarkEmoji = Emoji.fromUnicode ("✅");
+            final Emoji xEmoji = Emoji.fromUnicode ("❌");
+            stringSelectMenuBuilder.addOption (question.correct, question.correct, whiteCheckMarkEmoji);
+            for (final String wrongAnswer : question.wrong) {
+                stringSelectMenuBuilder.addOption (wrongAnswer, wrongAnswer, xEmoji);
+            }
+            final StringSelectMenu stringSelectMenu = stringSelectMenuBuilder.build ();
+            responseMessageCreateBuilder.addActionRow (stringSelectMenu);
+        }
+        final MessageCreateData messageCreateData = responseMessageCreateBuilder.build ();
+        final InteractionHook interactionHook = slashCommandInteractionEvent.getHook ();
+        final WebhookMessageCreateAction <Message> messageWebhookMessageCreateAction = interactionHook.sendMessage (messageCreateData);
+        messageWebhookMessageCreateAction.queue ();
     }
 }
